47707be7b90e9d2734d2ed9659469ea9fa184c2b
Artem Benda <12459262+artem-benda@users.noreply.github.com>
2024-04-13 19:15:49 +0300

    - промежуточный коммит

--- internal/application/handler/registerUser.go
+++ internal/application/handler/registerUser.go
@@ -3,7 +3,9 @@ package handler
 import (
 	"github.com/artem-benda/gophermart/internal/domain/service"
 	"github.com/artem-benda/gophermart/internal/infrastructure/dto"
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v3"
+	"net/http"
 )
 
 type RegisterUser struct {
@@ -25,6 +27,13 @@ func (h RegisterUser) RegisterUser(ctx fiber.Ctx) error {
 	err = b.JSON(userDTO)
 	if err != nil {
 		return err
+	}
+
+	v := validator.New()
+	err = v.Struct(userDTO)
+	if err != nil {
+		ctx.Response().SetStatusCode(http.StatusBadRequest)
+		return nil
 	}
 
 	err = h.svc.Register(ctx, userDTO.Login, userDTO.Password)
