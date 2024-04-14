1cd25b5c89beb9c5576c36c142678160cd7009d6
Artem Benda <12459262+artem-benda@users.noreply.github.com>
2024-04-14 16:48:02 +0300

    - промежуточный коммит

--- internal/application/handler/login.go
+++ internal/application/handler/login.go
@@ -2,11 +2,13 @@ package handler
 
 import (
 	"github.com/artem-benda/gophermart/internal/domain/service"
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v3"
 )
 
 type User struct {
-	svc service.User
+	svc      *service.User
+	validate *validator.Validate
 }
 
 func Login(ctx fiber.Ctx) error {
