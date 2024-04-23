33660719da7908ec230ce411e5b83447670f8775
Artem Benda <12459262+artem-benda@users.noreply.github.com>
2024-04-23 12:08:58 +0300

    промежуточный коммит

--- internal/infrastructure/dto/register_user_request.go
+++ internal/infrastructure/dto/register_user_request.go
@@ -1,6 +1,6 @@
 package dto
 
 type RegisterUserRequest struct {
-	Login    string `json:"login" validate:"required,min=3,max=255"`
-	Password string `json:"password" validate:"required,min=8,max=26"`
+	Login    string `json:"login" validate:"required,max=255"`
+	Password string `json:"password" validate:"required,max=255"`
 }
